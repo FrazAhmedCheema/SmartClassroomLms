0a7ebd4f29abd20a53cb78deb863faccf28e2fd5
Ayesha Asghar <211370024@gift.edu.pk>
2025-02-03 13:31:42 +0500

    Complete Sub-admin interface

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,24 +10,44 @@ import SubAdminLogin from './screens/sub-admin/SubAdminLogin';
 import SubAdminDashboard from './screens/sub-admin/SubAdminDashboard';
 import ManageStudents from './screens/sub-admin/ManageStudents';
 import ManageTeachers from './screens/sub-admin/ManageTeachers';
+import ViewClasses from './screens/sub-admin/ViewClasses';
+import ClassDetails from './screens/sub-admin/ClassDetails';
+import { ClassesProvider } from './context/ClassesContext';
 // ...import other components as needed...
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<RegisterInstitute />} />
-        <Route path="/admin/login" element={<AdminLogin />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-        <Route path="/admin/manage-institutes" element={<ManageInstitutes />} />
-        <Route path="/admin/manage-requests" element={<ManageRequests />} />
-        <Route path="/sub-admin/login" element={<SubAdminLogin />} />
-        <Route path="/sub-admin/dashboard" element={<SubAdminDashboard />} />
-        <Route path="/sub-admin/students" element={<ManageStudents />} />
-        <Route path="/sub-admin/teachers" element={<ManageTeachers />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
+    <ClassesProvider>
+      <Router>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<RegisterInstitute />} />
+          
+          {/* Admin Routes */}
+          <Route path="/admin">
+            <Route path="login" element={<AdminLogin />} />
+            <Route path="dashboard" element={<AdminDashboard />} />
+            <Route path="manage-institutes" element={<ManageInstitutes />} />
+            <Route path="manage-requests" element={<ManageRequests />} />
+          </Route>
+
+          {/* Sub-Admin Routes */}
+          <Route path="/sub-admin">
+            <Route path="login" element={<SubAdminLogin />} />
+            <Route path="dashboard" element={<SubAdminDashboard />} />
+            <Route path="students" element={<ManageStudents />} />
+            <Route path="teachers" element={<ManageTeachers />} />
+            <Route path="classes">
+              <Route index element={<ViewClasses />} />
+              <Route path=":id" element={<ClassDetails />} />
+            </Route>
+          </Route>
+
+          {/* Fallback */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    </ClassesProvider>
   );
 }
 
